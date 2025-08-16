3c3ef68bf1f29449d9853d45be3fc1a5cae2838d
Christian Carhuachin <cristiancarhuachin@gmail.com>
2025-08-16 00:46:12 -0500

    Creating functions to add tasks

---
+++ app.js
@@ -0,0 +1,27 @@
+const taskForm = document.getElementById('task-form');
+const taskList = document.getElementById('task-list');
+
+taskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const taskInput = document.getElementById('task-input');
+    const task = taskInput.value;
+    console.log('Tarea agregada:', task);
+    if(task) {
+        taskList.append(createTaskElement(task))
+        taskInput.value = ''
+    }
+});
+
+function createTaskElement(task) {
+    const li = document.createElement('li');
+    li.textContent = task;
+    li.append(createButton('❌','delete-btn'), createButton('✏️','edit-btn'));
+    return li;
+}
+
+function createButton(text, className) {
+    const btn = document.createElement('span');
+    btn.textContent = text;
+    btn.className = className;
+    return btn;
+}
